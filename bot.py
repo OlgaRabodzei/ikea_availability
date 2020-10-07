08cf9b9467f26648f108634c010d3b2600eca875
Olga Rabodzei <appossumik@gmail.com>
2020-10-07 19:11:40 +0300

    Split bot into two commands - check and forecast.

--- bot.py
+++ bot.py
@@ -5,12 +5,38 @@ from ikea_availability_api import IkeaAvailability
 bot = telebot.TeleBot(config.token)
 
 
+@bot.message_handler(commands=["forecast"])
+def forecast(message):
+    # TODO find better way to get parameter.
+    # TODO sanitize text message.
+    product_id = message.text[len("/forecast") + 1:]
+    if len(product_id) == 0:
+        bot.send_message(message.chat.id, 'Looks like you forgot a product ID.')
+        return
+    if len(product_id) != 8 or not product_id.isdigit():
+        bot.send_message(message.chat.id, 'Please, type only a product ID.')
+        return
+
+    api_service = IkeaAvailability(product_id)
+    forecast = api_service.availability_forecast()
+    # TODO Form pretty answer.
+    bot.send_message(message.chat.id, str(forecast))
+
+
 @bot.message_handler(content_types=["text"])
 def repeat_all(message):
     # TODO sanitize text message.
-    api_service = IkeaAvailability()
-    availability_response = api_service.get_availability_info(message.text)
-    bot.send_message(message.chat.id, str(availability_response))
+    product_id = message.text
+
+    if len(product_id) != 8 or not product_id.isdigit():
+        bot.send_message(message.chat.id, 'Please, type only a product ID.')
+        return
+    api_service = IkeaAvailability(product_id)
+    if api_service.is_product_available():
+        bot.send_message(message.chat.id, 'Happy to say the product is available at the stock!')
+    else:
+        bot.send_message(message.chat.id,
+                         f'Unfortunately the product is out of stock!\nTry to check a forecast for {product_id}')
 
 
 if __name__ == '__main__':
--- ikea_availability_api.py
+++ ikea_availability_api.py
@@ -1,25 +1,23 @@
 import requests
 from xml.etree import ElementTree
 
-# product_id = '50219044'
+# product_id_low_availability = '50219044'
+# product_id_high_availability = '90263902'
 
 
 class IkeaAvailability:
-    def __init__(self):
-        self.url_base = 'https://www.ikea.com/ua/uk/iows/catalog/availability/'
-
-    def _request_availability_info(self, product_id):
-        response = requests.get(self.url_base + product_id)
+    def __init__(self, product_id):
+        url_base = 'https://www.ikea.com/ua/uk/iows/catalog/availability/'
+        response = requests.get(url_base + product_id)
         if response.status_code != 200:
             # TODO better error handling.
-            print('Sorry! Service is an available.')
-            return False
-        # TODO Can the generator through an error?
-        return ElementTree.fromstring(response.content)
+            # raise Exception('Sorry! Service is an available.')
+            return
+        # TODO Can the generator raise an error?
+        self.response_content = ElementTree.fromstring(response.content)
 
-    @staticmethod
-    def _is_product_available(response_content):
-        for stock in response_content.findall('availability/localStore/stock'):
+    def is_product_available(self):
+        for stock in self.response_content.findall('availability/localStore/stock'):
             if stock.find('isSoldInStore') is None or stock.find('availableStock') is None:
                 continue
             # Does the product belongs to the store’s portfolio.
@@ -30,10 +28,9 @@ class IkeaAvailability:
                 return True
         return False
 
-    @staticmethod
-    def _availability_forecast(response_content):
+    def availability_forecast(self):
         forecast_dates = {}
-        for stock in response_content.findall('availability/localStore/stock'):
+        for stock in self.response_content.findall('availability/localStore/stock'):
             if stock.find('forecasts') is None:
                 continue
             forecast_dates = {
@@ -43,12 +40,3 @@ class IkeaAvailability:
             }
 
         return forecast_dates
-
-    def get_availability_info(self, product_id):
-        # TODO Split into operations?
-        response_content = self._request_availability_info(product_id)
-        if self._is_product_available(response_content):
-            return 'Happy to say that the product is available in stock!'
-        else:
-            # TODO Form a string.
-            return self._availability_forecast(response_content)
